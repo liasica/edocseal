25d08b1d27ac65a85c15fc2c51479ef19ae3e93b
JorjanLLT <jorjanlawliet@outlook.com>
2024-04-12 09:44:37 +0800

    加入机构签发证书请求实现

--- internal/biz/ca.go
+++ internal/biz/ca.go
@@ -5,10 +5,17 @@
 package biz
 
 import (
+	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/base64"
+	"encoding/hex"
 	"errors"
 	"fmt"
+
+	"github.com/go-resty/resty/v2"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/tjfoc/gmsm/sm3"
+	"go.uber.org/zap"
 
 	"github.com/liasica/edocseal/ca"
 	"github.com/liasica/edocseal/internal/g"
@@ -20,6 +27,11 @@ func RequestCertificae(paths *model.DocumentPaths, name, province, city, address
 	var crt, key []byte
 	if g.IsSelfSign() {
 		crt, key, err = selfIssueCertificate(name, province, city, address, phone, idcard)
+		if err != nil {
+			return
+		}
+	} else {
+		crt, key, err = agencyIssueCertificate(name, province, city, address, phone, idcard)
 		if err != nil {
 			return
 		}
@@ -62,7 +74,7 @@ func selfIssueCertificate(name, province, city, address, phone, idcard string) (
 func agencyIssueCertificate(name, province, city, address, phone, idcard string) (crt, key []byte, err error) {
 	// 生成私钥
 	priKey := ca.GenerateRsaPrivateKey()
-
+	key, _ = x509.MarshalPKCS8PrivateKey(key)
 	// 生成CSR请求
 	var csr []byte
 	csr, err = ca.GenerateRequest(priKey, pkix.Name{
@@ -71,8 +83,47 @@ func agencyIssueCertificate(name, province, city, address, phone, idcard string)
 	})
 
 	pkcs10 := base64.StdEncoding.EncodeToString(csr)
-	fmt.Println(pkcs10)
+	param := make(map[string]string)
+	param["alg"] = "RSA"
+	param["certType"] = "1"
+	param["province"] = province
+	param["city"] = city
+	param["agent"] = name
+	param["agentTel"] = phone
+	param["agentNumber"] = idcard
+	param["clientName"] = name
+	param["legalNumber"] = idcard
+	param["legalPersonTel"] = phone
+	param["companyAdd"] = address
+	param["prjId"] = "401"
+	param["pkcs10"] = pkcs10
 
-	// TODO: 待实现请求机构签发证书，model放到model/ca.go中
+	h := sm3.New()
+	h.Write([]byte(param["agentNumber"] + "_" + param["clientName"]))
+	sum := h.Sum(nil)
+	param["passwdDigest"] = hex.EncodeToString(sum)
+
+	var resp *resty.Response
+	resp, err = resty.New().R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(param).
+		SetResult(&model.AgencyCertResponse{}).
+		Post("http://117.32.132.76:28998/boss-eai/cert/getCert")
+	if err != nil {
+		zap.L().Fatal("机构签发证书失败", zap.Error(err))
+		return
+	}
+	res := new(model.AgencyCertResponse)
+	err = jsoniter.Unmarshal(resp.Body(), res)
+	if err != nil {
+		zap.L().Fatal("机构签发证书失败", zap.Error(err))
+		return
+	}
+	if res.ErrorCode != "200" || res.Result == "false" {
+		zap.L().Fatal("机构签发证书失败", zap.Error(errors.New(res.ErrorMessage)))
+		return nil, nil, errors.New(res.ErrorMessage)
+	}
+
+	crt, err = base64.StdEncoding.DecodeString(res.JsonObj.SignCert)
 	return
 }
--- internal/model/ca.go
+++ internal/model/ca.go
@@ -3,3 +3,14 @@
 // Created at 2024-04-11, by liasica
 
 package model
+
+type AgencyCertResponse struct {
+	ErrorCode    string        `json:"errorCode"`    // 200为正常，500/ E300000为错误
+	Result       string        `json:"result"`       // true为成功，false为失败
+	ErrorMessage string        `json:"errorMessage"` // 200为正常，500/ E300000为错误
+	JsonObj      JsonObjStruct `json:"jsonObj"`      // 签名公钥证书，cer格式 obj
+}
+
+type JsonObjStruct struct {
+	SignCert string `json:"signCert"` // 签名公钥证书，cer格式
+}
