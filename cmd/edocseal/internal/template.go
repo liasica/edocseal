2357ac1098d20c635f6956f5af66ce6626570f08
liasica <magicrolan@qq.com>
2025-06-25 19:29:47 +0800

    自动扫描模板

--- cmd/edocseal/internal/template.go
+++ cmd/edocseal/internal/template.go
@@ -17,135 +17,233 @@ import (
 	"auroraride.com/edocseal/internal/model"
 )
 
-func templateCommand() *cobra.Command {
+func generateTemplate(pdf, form, templates string) {
+	// 获取模板文件md5
+	templateId, err := edocseal.FileMd5(pdf)
+	if err != nil {
+		fmt.Printf("模板MD5获取失败: %v\n", err)
+		os.Exit(1)
+	}
+
+	templateId = strings.ToUpper(templateId)
+
+	// 重命名模板文档
+	templateFile, _ := filepath.Abs(filepath.Join(templates, templateId+".pdf"))
+	if !edocseal.FileExists(templateFile) {
+		err = copyFileContents(pdf, templateFile)
+		if err != nil {
+			fmt.Printf("模板文件复制失败: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// 获取表单属性
+	var b []byte
+	b, err = edocseal.Exec("qpdf", form, "--json")
+	if err != nil {
+		fmt.Printf("模板表单解析失败: %v\n", err)
+		os.Exit(1)
+	}
+
+	// 获取表单数据
+	var fd edocseal.Form
+	err = jsoniter.Unmarshal(b, &fd)
+	if err != nil {
+		fmt.Printf("表单数据解析失败: %v\n", err)
+		os.Exit(1)
+	}
+
+	// 生成模板ID
+	template := model.Template{
+		ID:     templateId,
+		File:   templateFile,
+		Fields: make(map[string]model.TemplateField),
+	}
+
+	// 模板数据
+	var hasEnt, hasRider bool
+	for _, field := range fd.Acroform.Fields {
+		m := fd.Objects[1]["obj:"+field.Annotation.Object]
+		mb, _ := jsoniter.Marshal(m)
+		var data edocseal.FormFieldObject
+		_ = jsoniter.Unmarshal(mb, &data)
+
+		fmt.Printf("%20.20s %20.20s\t%10.10s\t\t%6.2f, %6.2f, %6.2f, %6.2f\n",
+			field.Fullname,
+			field.Alternativename,
+			field.Fieldtype,
+			data.Value.Rect[0],
+			data.Value.Rect[1],
+			data.Value.Rect[2],
+			data.Value.Rect[3],
+		)
+		// if _, ok := fields[field.Fullname]; ok {
+		// 	fmt.Printf("字段重复: %s\n", field.Fullname)
+		// 	os.Exit(1)
+		// }
+
+		var typ model.TemplateFieldType
+		typ, err = model.NewTemplateFieldType(field.Fieldtype)
+		if err != nil {
+			fmt.Printf("字段类型解析失败: %v\n", err)
+			os.Exit(1)
+		}
+
+		if field.Fullname == model.EntSignField {
+			hasEnt = true
+		}
+
+		if field.Fullname == model.PersonalSignField {
+			hasRider = true
+		}
+
+		template.Fields[field.Fullname] = model.TemplateField{
+			Page: field.Pageposfrom1,
+			Type: typ,
+			Rectangle: &model.TemplateRectangle{
+				LeftBottom: model.Coordinate{
+					X: data.Value.Rect[0],
+					Y: data.Value.Rect[1],
+				},
+				RightTop: model.Coordinate{
+					X: data.Value.Rect[2],
+					Y: data.Value.Rect[3],
+				},
+			},
+		}
+	}
+
+	// 判定是否有签名字段
+	if !hasEnt {
+		fmt.Printf("模板没有签名字段: %s\n", model.EntSignField)
+		os.Exit(1)
+	}
+	if !hasRider {
+		fmt.Printf("模板没有签名字段: %s\n", model.PersonalSignField)
+		os.Exit(1)
+	}
+
+	// 存储模板配置
+	json, _ := jsoniter.MarshalIndent(template, "", "  ")
+	_ = os.WriteFile(filepath.Join(templates, fmt.Sprintf("%s.json", templateId)), json, 0755)
+	fmt.Printf("完成模板创建，模板ID: %s\n", templateId)
+}
+
+type Template struct {
+}
+
+func NewTemplate() *Template {
+	return &Template{}
+}
+
+func (t *Template) Group() *cobra.Group {
+	return &cobra.Group{
+		ID:    "template",
+		Title: "模板",
+	}
+}
+
+func (t *Template) Command() *cobra.Command {
+	cmd := &cobra.Command{
+		GroupID:           t.Group().ID,
+		Use:               "template",
+		Short:             "模板相关命令",
+		CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
+	}
+
+	cmd.AddCommand(
+		t.add(),
+		t.scan(),
+	)
+
+	return cmd
+}
+
+func (*Template) add() *cobra.Command {
 	var (
-		form string
-		path string
+		form      string
+		templates string
 	)
 
 	cmd := &cobra.Command{
-		Use:               "template <input>",
-		Short:             "添加模板\ninput: 空白合同文件\n  --form: 表单文件\n  --path: 模板路径",
-		Example:           "edocseal template ./contract.pdf --form ./form.pdf --path ./templates",
+		Use:               "add <input>",
+		Short:             "添加模板",
+		Example:           "edocseal template add ./contract.pdf --form ./form.pdf --path ./templates",
 		Args:              cobra.ExactArgs(1),
 		CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
 		Run: func(_ *cobra.Command, args []string) {
-			// 获取模板文件md5
-			templateId, err := edocseal.FileMd5(args[0])
-			if err != nil {
-				fmt.Printf("模板MD5获取失败: %v\n", err)
-				os.Exit(1)
-			}
-
-			templateId = strings.ToUpper(templateId)
-
-			// 重命名模板文档
-			templateFile, _ := filepath.Abs(filepath.Join(path, templateId+".pdf"))
-			if !edocseal.FileExists(templateFile) {
-				err = copyFileContents(args[0], templateFile)
-				if err != nil {
-					fmt.Printf("模板文件复制失败: %v\n", err)
-					os.Exit(1)
-				}
-			}
-
-			// 获取表单属性
-			var b []byte
-			b, err = edocseal.Exec("qpdf", form, "--json")
-			if err != nil {
-				fmt.Printf("模板表单解析失败: %v\n", err)
-				os.Exit(1)
-			}
-
-			// 获取表单数据
-			var fd edocseal.Form
-			err = jsoniter.Unmarshal(b, &fd)
-			if err != nil {
-				fmt.Printf("表单数据解析失败: %v\n", err)
-				os.Exit(1)
-			}
-
-			// 生成模板ID
-			template := model.Template{
-				ID:     templateId,
-				File:   templateFile,
-				Fields: make(map[string]model.TemplateField),
-			}
-
-			// 模板数据
-			var hasEnt, hasRider bool
-			for _, field := range fd.Acroform.Fields {
-				m := fd.Objects[1]["obj:"+field.Annotation.Object]
-				mb, _ := jsoniter.Marshal(m)
-				var data edocseal.FormFieldObject
-				_ = jsoniter.Unmarshal(mb, &data)
-
-				fmt.Printf("%20.20s %20.20s\t%10.10s\t\t%6.2f, %6.2f, %6.2f, %6.2f\n",
-					field.Fullname,
-					field.Alternativename,
-					field.Fieldtype,
-					data.Value.Rect[0],
-					data.Value.Rect[1],
-					data.Value.Rect[2],
-					data.Value.Rect[3],
-				)
-				// if _, ok := fields[field.Fullname]; ok {
-				// 	fmt.Printf("字段重复: %s\n", field.Fullname)
-				// 	os.Exit(1)
-				// }
-
-				var typ model.TemplateFieldType
-				typ, err = model.NewTemplateFieldType(field.Fieldtype)
-				if err != nil {
-					fmt.Printf("字段类型解析失败: %v\n", err)
-					os.Exit(1)
-				}
-
-				if field.Fullname == model.EntSignField {
-					hasEnt = true
-				}
-
-				if field.Fullname == model.PersonalSignField {
-					hasRider = true
-				}
-
-				template.Fields[field.Fullname] = model.TemplateField{
-					Page: field.Pageposfrom1,
-					Type: typ,
-					Rectangle: &model.TemplateRectangle{
-						LeftBottom: model.Coordinate{
-							X: data.Value.Rect[0],
-							Y: data.Value.Rect[1],
-						},
-						RightTop: model.Coordinate{
-							X: data.Value.Rect[2],
-							Y: data.Value.Rect[3],
-						},
-					},
-				}
-			}
-
-			// 判定是否有签名字段
-			if !hasEnt {
-				fmt.Printf("模板没有签名字段: %s\n", model.EntSignField)
-				os.Exit(1)
-			}
-			if !hasRider {
-				fmt.Printf("模板没有签名字段: %s\n", model.PersonalSignField)
-				os.Exit(1)
-			}
-
-			// 存储模板配置
-			json, _ := jsoniter.MarshalIndent(template, "", "  ")
-			_ = os.WriteFile(filepath.Join(path, fmt.Sprintf("%s.json", templateId)), json, 0755)
-			fmt.Printf("完成模板创建，模板ID: %s\n", templateId)
+			generateTemplate(args[0], form, templates)
 		},
 	}
 
-	cmd.Flags().StringVarP(&form, "form", "f", "", "表单文件")
-	cmd.Flags().StringVarP(&path, "path", "p", "templates", "模板路径")
+	cmd.Flags().StringVarP(&form, "form", "f", "", "表单模板")
+	cmd.Flags().StringVarP(&templates, "templates", "t", "./templates", "模板路径")
 
-	_ = cmd.MarkFlagRequired("temp")
+	_ = cmd.MarkFlagRequired("form")
+
+	return cmd
+}
+
+func (*Template) scan() *cobra.Command {
+	var (
+		templates string
+		force     bool
+	)
+
+	cmd := &cobra.Command{
+		Use:               "scan <input>",
+		Short:             "扫描目录生成模板",
+		Example:           "edocseal template scan ./runtime",
+		Args:              cobra.ExactArgs(1),
+		CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
+		Run: func(_ *cobra.Command, args []string) {
+			// generateTemplate(args[0], form, templates)
+			// 扫描目录
+			files, err := os.ReadDir(args[0])
+			if err != nil {
+				fmt.Printf("读取目录失败: %v\n", err)
+				os.Exit(1)
+			}
+
+			for _, f := range files {
+				// 跳过目录、非PDF后缀文件、表单文件
+				if f.IsDir() || !strings.HasSuffix(f.Name(), ".pdf") || strings.HasSuffix(f.Name(), "-form.pdf") {
+					continue
+				}
+
+				// 检查是否存在表单文件
+				name := strings.TrimSuffix(f.Name(), filepath.Ext(f.Name()))
+				form := filepath.Join(args[0], fmt.Sprintf("%s-form.pdf", name))
+				if !edocseal.FileExists(form) {
+					continue
+				}
+
+				// 获取合同模板ID
+				pdf := filepath.Join(args[0], f.Name())
+				var templateId string
+				templateId, err = edocseal.FileMd5(pdf)
+				if err != nil {
+					fmt.Printf("合同模板MD5获取失败: %v\n", err)
+					os.Exit(1)
+				}
+				templateId = strings.ToUpper(templateId)
+
+				// 检查模板文件是否存在
+				templateFile := filepath.Join(templates, fmt.Sprintf("%s.pdf", templateId))
+				if edocseal.FileExists(templateFile) && !force {
+					fmt.Printf("模板文件已存在: %s\n", templateId)
+					continue
+				}
+
+				// 生成模板
+				fmt.Printf("生成模板: %s\n", templateId)
+				generateTemplate(pdf, form, templates)
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&templates, "templates", "t", "./templates", "模板路径")
+	cmd.Flags().BoolVarP(&force, "force", "f", false, "是否强制覆盖已存在的模板")
 
 	return cmd
 }
